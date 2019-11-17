d27e79fa3a9be9609a19200f6da6ffd30ef09bcb
manivasagam <qatest@ADFL-1121.ADFDATA.NET>
2019-11-17 23:41:30 +0530

    Question added

--- RepeatCharacter.js
+++ RepeatCharacter.js
@@ -1,3 +1,8 @@
+/* Eg 1: Input: a1b10
+       Output: abbbbbbbbbb
+Eg: 2: Input: b3c6d15
+          Output: bbbccccccddddddddddddddd
+The number varies from 1 to 99. */
 let GivenString="a10b20c2";
 let CharacterList=GivenString.split(/[0-99]/).filter(function(val){return val!=""})
 let NumberList=GivenString.split(/[a-z]/).filter(function(val){return val!=""})
